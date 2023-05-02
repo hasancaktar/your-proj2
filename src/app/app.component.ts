9af1caeda2a16ad5c3b0519c116ff51a63b3070f
Hasan Sancaktar <hasancaktar@gmail.com>
2023-05-02 15:54:40 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template:`
+    <input type="text" [value]="title">
+    <app-home pageName="ascfgbhgfd"></app-home>
+    <input type="text" [(ngModel)]="name">
+    <ul>
+      <li *ngFor="let name of names; index as s">
+{{name}} - {{s}}
+      </li>
+    </ul>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  names: string[]=["g","q","w","ı","b"]
   title = 'your-proj2';
+  name:string
 }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  //templateUrl: './home.component.html',
+  
+  template:`
+  <h1>{{pageName}}</h1>
+  `,
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+@Input() pageName: string;
+}
